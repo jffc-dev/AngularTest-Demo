8514c59fd70f6c7a1c4312c25f20a276075ccb36
Javier Flores <jffc.dev@gmail.com>
2026-07-13 17:50:04 -0500

    feat: integrate contador-antes and contador-con-signals components with updated templates and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContadorAntes } from './components/contador-antes/contador-antes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ContadorAntes],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
